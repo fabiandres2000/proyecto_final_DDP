35a8ac05d1a623b66141a2bee75e103450cc83a8
juan102 <amarisj77@gmail.com>
2020-05-27 20:45:21 -0500

    Api Detectar

---
+++ WebApi/Controllers/DetectarEnfermedadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Services;
+using Domain.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DetectarEnfermedadController : ControllerBase
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+        //Se Recomienda solo dejar la Unidad de Trabajo
+        public DetectarEnfermedadController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public ActionResult<DetectarResponseapp> Post(DetectarRequestapp request)
+        {
+            DetectarEnfermedadServiceApp _service = new DetectarEnfermedadServiceApp(_unitOfWork);
+            DetectarResponseapp response = _service.detectar(request);
+            return Ok(response);
+        }
+    }
+}
