9461669701f15d3171a7b074434d9170e84fa9c5
fabiandres2000 <39952571+fabiandres2000@users.noreply.github.com>
2020-05-29 22:10:00 -0500

    ConsultarEnfermedadSintomaService
    ConsultarEnfermedadSintomaService

---
+++ Application/Services/ConsultarEnfermedadSintomaService.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Contracts;
+using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Application.Services
+{
+    public class ConsultarEnfermedadSintomaService
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+
+        public ConsultarEnfermedadSintomaService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        
+        }
+
+        public List<ConsultarEnfermedadSintomaResponse> Consultar()
+        {
+            List<ConsultarEnfermedadSintomaResponse> lista = new List<ConsultarEnfermedadSintomaResponse>();
+            var enfermedadsintoma = _unitOfWork.IEnfermedadSintoma.FindBy(includeProperties: "Sintoma,Enfermedad").ToList();
+            foreach (var Item in enfermedadsintoma)
+            { 
+                ConsultarEnfermedadSintomaResponse co = new ConsultarEnfermedadSintomaResponse();
+                co.Enfermedad = Item.Enfermedad;
+                co.Sintoma = Item.Sintoma;
+                lista.Add(co);
+            }
+            return lista;
+        }
+        public class ConsultarEnfermedadSintomaResponse
+        {
+            public Sintoma Sintoma { get; set; }
+            public Enfermedad Enfermedad { get; set; }
+
+        }
+    }
+}
